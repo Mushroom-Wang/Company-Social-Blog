8c5035fd05e40a8834481812d891952a16b08588
Quan Wang <quanwang452@gmail.com>
2022-07-26 15:15:12 -0400

    create error pages and error handler

--- kittencompanyblog/__init__.py
+++ kittencompanyblog/__init__.py
@@ -1,6 +1,9 @@
+from kittencompanyblog.error_pages.handlers import error_pages
 from kittencompanyblog.core.views import core
 from flask import Flask
 
 app = Flask(__name__)
 
 app.register_blueprint(core)
+
+app.register_blueprint(error_pages)
