d9fb06fd841efc1957dc52f26b5074cd110f69bf
Quan Wang <quanwang452@gmail.com>
2022-07-26 16:57:16 -0400

    set secret key, revise nav bar, and add requirements.txt for all dependencies

--- kittencompanyblog/__init__.py
+++ kittencompanyblog/__init__.py
@@ -9,6 +9,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecret'
+
 ############################
 ### DATABASE SETUP ##########
 ########################
